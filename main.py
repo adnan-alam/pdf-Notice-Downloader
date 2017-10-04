41367a918a8e8e329e23653a59766917ec7de7f8
Adnan Alam <adnan320324@gmail.com>
2017-10-04 13:46:46 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup, SoupStrainer
+from urllib.request import urlretrieve
+
+
+notice_page = requests.get("url of notice page")
+
+# e.g. ,
+#cu_admission_notice = requests.get("http://admission.eis.cu.ac.bd/index.php?act=information/get_notices/all")
+#kuet_admission_notice = requests.get("http://admission.kuet.ac.bd/Notice.aspx")
+
+
+parsed_html = BeautifulSoup(notice_page.text,"lxml")
+
+#e.g. ,
+#parsed_html = BeautifulSoup(cu_admission_notice.text,"lxml")
+#parsed_html = BeautifulSoup(kuet_admission_notice.text,"lxml")
+
+
+all_links = parsed_html.find_all("a")
+
+
+# list of download links of notices
+download_links = []
+
+for link in all_links:
+    if ".pdf" in link["href"]:
+        if "website-url" in link["href"]:
+        # e.g. if "http://admission.kuet.ac.bd/" in link["href"]:
+            download_links.append(link["href"])
+        else:
+            download_links.append("website-url" + link["href"])
+            # e.g. download_links.append("http://admission.kuet.ac.bd/" + link["href"])
+        
+
+# downloader
+for link in download_links:
+    extension = link.split("/")[-1]
+    urlretrieve(link,"directory path/{}".format(extension))
+    # e.g. urlretrieve(link,"home/user/notices_folder/{}".format(extension))
+
